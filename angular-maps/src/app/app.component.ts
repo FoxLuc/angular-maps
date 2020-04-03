9e9f1d3b78dafe0d483a587deceddf8f9282a72d
FoxLuc <lucaf8502@gmail.com>
2020-04-03 08:14:15 +0000

    01.0

---
+++ angular-maps/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-maps';
+  lat: number = 45.506738;
+  lng: number = 9.190766;
+}
